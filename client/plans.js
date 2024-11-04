b6c2843845c975dad761861fa661f319aebba7f9
gXLg <natgaev@gmail.com>
2024-11-04 19:19:36 +0100

    patch up

--- client/plans.js
+++ client/plans.js
@@ -5,13 +5,13 @@ d => "<table><tr><th>Day</th><th>Time</th><th>Activity</th>" +
     let c = "";
     if (d == p.day) {
       c = " style='background-color: lightgray;";
-      const [h1, m1, h2, m2] = p.time.match(/\d\d/).map(i => parseInt(i));
+      const [h1, m1, h2, m2] = p.time.match(/\d\d/g).map(i => parseInt(i));
       const h = n.getHours();
       const m = n.getMinutes();
 
       const [t1, t, t2] = [h1 * 60 + m1, h * 60 + m, h2 * 60 + m2];
 
-      if (t1 <= t && t <= t2) c += " color: green;";
+      if (t1 <= t && (t <= t2 || t2 <= t1)) c += " color: green;";
       c += "'";
     }
     return "<tr" + c + "><td>" + p.day + "</td><td>" + p.time + "</td><td>" + p.name + "</td></tr>";
